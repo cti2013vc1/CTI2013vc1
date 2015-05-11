56299b4f6016071667a55d27c0f546df5454ae8e
kassiawillers <kassiawillers@gmail.com>
2015-05-11 13:44:37 -0300

    kjkjk

---
+++ Projeto/Projeto/untConsultaPeriodosLetivos.dfm
@@ -0,0 +1,5 @@
+﻿inherited frmConsultaPeríodosLetivos: TfrmConsultaPeríodosLetivos
+  Caption = 'Consulta de Per'#237'odos Letivos'
+  PixelsPerInch = 96
+  TextHeight = 13
+end
---
+++ Projeto/Projeto/untConsultaPeriodosLetivos.pas
@@ -0,0 +1,25 @@
+unit untConsultaPeriodosLetivos;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, unt_modeloConsulta, Vcl.Grids,
+  Vcl.DBGrids, Vcl.Buttons, Vcl.StdCtrls, Vcl.Imaging.pngimage, Vcl.ExtCtrls;
+
+type
+  TfrmConsultaPeríodosLetivos = class(TForm1)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmConsultaPeríodosLetivos: TfrmConsultaPeríodosLetivos;
+
+implementation
+
+{$R *.dfm}
+
+end.
